8a1720ae60d72fc236f64390cdc5d04ff8e1b9ef
Dev-Data-Net <dev.data.net@gmail.com>
2024-05-09 22:36:15 +0200

    Add project files.

---
+++ EvenOddNumber/Program.cs
@@ -0,0 +1,41 @@
+﻿Console.WriteLine("Sprawdzamy czy podana przez Ciebie liczba jest parzysta lub nieparzysta");
+Console.WriteLine("Podaj dowolną liczbę: ");
+
+
+
+while (true)
+{
+    var number = AddNumber();
+
+    if (number % 2 == 0)
+    {
+        Console.WriteLine("Liczba jest parzysta.");
+    }
+    else
+    {
+        Console.WriteLine("Liczba jest nieparzysta");
+    }
+
+    Console.WriteLine("\nPodaj następną liczbę: \nJeżeli chcesz zakończyć naciśnij 'q'");
+}
+
+
+static int AddNumber()
+{
+    while (true)
+    {
+        var val = Console.ReadLine();
+        if (val.ToUpper() == "Q")
+        {
+            Environment.Exit(0);
+        }
+
+        if (!int.TryParse(val, out int number))
+        {
+            Console.WriteLine("Podano nieprawidłową wartość. Spróbuj ponownie - podaj dowolną liczbę: ");
+            continue;
+
+        }
+        return number;
+    }
+}
